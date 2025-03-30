e6b5b550e2e198672db385c0bcfbddab34ec440d
bernardoolv <bernardo.oliveira.1468416@sga.pucminas.br>
2025-03-30 18:28:56 -0300

    Implementação inicial do gerenciador de rotas

--- src/index.php
+++ src/index.php
@@ -5,8 +5,8 @@ require __DIR__.'/vendor/autoload.php';
 use \App\Http\Router;
 use \App\Controller\Home;
 
-define('URL', 'http://localhost/eco-gestor');
-$obRouter = new Router(URL);
+// define('URL', 'http://localhost/eco-gestor');
+// $obRouter = new Router(URL);
 
-echo '<pre>'; print_r($obRouter);echo '</pre>';exit;
+// echo '<pre>'; print_r($obRouter);echo '</pre>';exit;
 echo Home::index();
