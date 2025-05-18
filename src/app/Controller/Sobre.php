7668208b2442b452088dbb135c2da5837b5ba80f
Diogo Marques <1462675@sga.pucminas.br>
2025-05-18 16:37:45 -0300

    Sobre nós

--- src/app/Controller/Sobre.php
+++ src/app/Controller/Sobre.php
@@ -11,8 +11,11 @@ class Sobre extends AbstractController{
         try {
                         
             $data = [
-                'titulo' => 'Tela Sobre teste',
-                'descricao' => 'Estudantes da PUC Minas - Diogo',
+                'titulo' => 'Sobre nós',
+                'descricao1' => 'O EcoGestor nasceu com a missão de transformar a maneira como empresas e condomínios lidam com seus resíduos sólidos. Sabemos que o descarte incorreto de materiais é um dos grandes desafios ambientais da atualidade, e por isso desenvolvemos uma plataforma que vai além das instruções tradicionais.',
+                'descricao2' => 'Nosso sistema oferece informações claras e práticas sobre reciclagem e compostagem, além de fornecer dicas personalizadas com o apoio da inteligência artificial Gemini. Essa integração com IA permite que os usuários tirem dúvidas em tempo real e recebam sugestões conforme sua realidade, promovendo mudanças conscientes no dia a dia.',
+                'descricao3' => 'Acreditamos que a sustentabilidade começa com pequenas atitudes — e nossa proposta é tornar essas atitudes mais fáceis e acessíveis para todos. Com uma interface intuitiva e conteúdo educativo, o EcoGestor é voltado tanto para gestores quanto para moradores e funcionários, apoiando iniciativas sustentáveis em todos os níveis.',
+                'descricao4' => 'Nosso compromisso é claro: contribuir para um futuro mais limpo, consciente e responsável.',
             ];
             
         } catch (\Throwable $th) {
