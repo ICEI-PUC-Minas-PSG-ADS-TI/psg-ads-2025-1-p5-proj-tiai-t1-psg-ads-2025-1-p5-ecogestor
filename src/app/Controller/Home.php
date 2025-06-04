70258c34337c8b7b1fcfb47e726146158dcb3e5f
ArthurH0 <arthur.acacio@hotmail.com>
2025-06-03 21:43:18 -0300

    #Arthur Create Tela Home

--- src/app/Controller/Home.php
+++ src/app/Controller/Home.php
@@ -11,27 +11,16 @@ class Home extends AbstractController{
     public static function index(){
         $data = [];
         try {
-            $api = new APINews();
-            
-            $noticia = $api->Get('Compostagem')['noticias'][0];
-            if($noticia){
-
                 $data = [
-                    'titulo' => $noticia['title'],
-                    'imagem' => $noticia['urlToImage'],
-                    'url'    => $noticia['url'],
-                    'autor'  => $noticia['source']['name'],
-                    'descricao' => $noticia['description'],
-                    'data'      => date('d/m/Y', strtotime($noticia['publishedAt']))
+                    'texto' => 'A Ecogestor é uma empresa comprometida com o futuro do planeta. Atuamos no ramo da reciclagem, promovendo soluções sustentáveis para a gestão de resíduos, com foco na preservação ambiental e na conscientização da sociedade.
+                                Nosso objetivo vai além da coleta e reaproveitamento de materiais. Aqui, você encontra dicas práticas para tornar seu dia a dia mais sustentável, além de notícias atualizadas sobre o universo da reciclagem, meio ambiente e inovações ecológicas.'
                 ];
-            }            
+                      
             
         } catch (\Throwable $th) {
             echo($th->getMessage());
         }
         $conteudo = View::render('home', $data);
-
-        //parametros(tituloPag, conteudo)
         return self::getBase('Home', $conteudo);
 
     }
