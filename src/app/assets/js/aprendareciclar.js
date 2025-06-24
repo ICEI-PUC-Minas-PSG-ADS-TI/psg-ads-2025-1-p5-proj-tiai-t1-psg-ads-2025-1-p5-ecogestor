2633444e00e2203f55a8fb985d1817064af91d85
KloseBH <danielhabdias@gmail.com>
2025-06-24 19:53:19 -0300

    #Daniel - Adjust: Correção de pdf para botão não aparecer.

--- src/app/assets/js/aprendareciclar.js
+++ src/app/assets/js/aprendareciclar.js
@@ -105,9 +105,8 @@ var exportarParaPDF = () => {
     const logoImg = new Image();
     logoImg.src = 'app/assets/images/logo-ecogestor.png';
 
-    botao.style.display = 'none'; 
-
     logoImg.onload = function () {
+        botao.style.display = 'none'; 
         html2canvas(elemento).then(canvas => {
             const imgData = canvas.toDataURL('image/png');
 
